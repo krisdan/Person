a896a2f545b52783c3155009c40f89d183eddf18
chrisKrisdan <chris.krisdan@gmail.com>
2015-07-09 14:36:35 +0100

    First Commit to the Master Branch Version 1.0.0

---
+++ src/Person/Main.java
@@ -0,0 +1,16 @@
+package Person;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Person dave = new Person("David");
+		
+		System.out.println(dave.name);
+
+	}
+
+}
---
+++ src/Person/Person.java
@@ -0,0 +1,11 @@
+package Person;
+
+public class Person {
+	
+	public String name;
+
+	public Person(String name) {
+		
+	}
+
+}
