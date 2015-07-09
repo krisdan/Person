cc64ef3626d5d5b6d807689f93ac9ecde20ade01
chrisKrisdan <chris.krisdan@gmail.com>
2015-07-09 19:00:34 +0100

    Main.main uses String.length(), Arrays, ints for loop, foreach loop, variable assignment and Object instanciation.

--- src/Person/Main.java
+++ src/Person/Main.java
@@ -8,9 +8,53 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Person dave = new Person("David");
+		String[] firstNames = {"Dan","Chris","Dave","Bob","Ben","Steve","Carl","John","Kevin","muhammad"};
 		
-		System.out.println(dave.name);
+		String[] lastNames = new String[10];
+		
+		lastNames[0] = "Perkins";
+		  lastNames[1] = "Smith"; 
+		  lastNames[2] = "Jones"; 
+		  lastNames[3] = "Schmidt"; 
+		  lastNames[4] = "Sing"; 
+		  lastNames[5] = "Thorpe"; 
+		  lastNames[6] = "Watts"; 
+		  lastNames[7] = "Evans"; 
+		  lastNames[8] = "Jenkins"; 
+		  lastNames[9] = "Green";
+		
+		System.out.println(lastNames[5]);
+		
+		String name = "john";
+		
+		System.out.println(name.length());
+		
+		Person person1 = new Person(name);
+		
+		System.out.println(person1.name);
+		
+		int i;
+		
+		Person[] people = new Person[10];
+		
+		for(i = 0; i < 10; i++) {
+			
+			int fNameRef = 9 - i;
+			String firstName = firstNames[fNameRef];
+			
+			int lNameRef = 9 - firstName.length();
+			
+			String lastName = lastNames[lNameRef];
+			
+			String fullName = firstName + lastName;
+			
+			people[i] = new Person(fullName);
+		}
+		
+		for(Person person : people) {
+			
+			System.out.println(person.toString());
+		}
 
 	}
 
