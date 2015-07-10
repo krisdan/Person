d901d3b0abc73625b69a0ead7fae98abbb6be394
chrisKrisdan <chris.krisdan@gmail.com>
2015-07-10 10:19:45 +0100

    Began dividing client code into sensible sections and adding educative comments.

--- src/Person/Main.java
+++ src/Person/Main.java
@@ -1,61 +1,124 @@
 package Person;
+import java.util.*;
 
-public class Main {
+public class Main
+{
 
-	public Main() {
+	public Main()
+	{
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
-		String[] firstNames = {"Dan","Chris","Dave","Bob","Ben","Steve","Carl","John","Kevin","muhammad"};
+		/*
+		 * JAVA VARIABLE INITIALISATION 
+		 * AND OBJECT INSTANCIATION.
+		 */
+		 
+		 /*
+		  * VARIABLE INITIALISATION.
+		  */
 		
-		String[] lastNames = new String[10];
+		/*
+		 * Declare an empty String variable.
+		 */
+		 String exerciseTitle;
+		 
+		 /*
+		  * Assign a value to the exerciseTitle variable.
+		  */
+		 exerciseTitle = "Java Variable initialisation and Object instanciation.";
 		
-		lastNames[0] = "Perkins";
-		  lastNames[1] = "Smith"; 
-		  lastNames[2] = "Jones"; 
-		  lastNames[3] = "Schmidt"; 
-		  lastNames[4] = "Sing"; 
-		  lastNames[5] = "Thorpe"; 
-		  lastNames[6] = "Watts"; 
-		  lastNames[7] = "Evans"; 
-		  lastNames[8] = "Jenkins"; 
-		  lastNames[9] = "Green";
-		
-		System.out.println(lastNames[5]);
-		
+        /*
+		 * Declare a String and assign it a value in a primitive style.
+		 */
 		String name = "john";
 		
-		System.out.println(name.length());
+		/*
+		 * Although Strings are often initialised in a similar way to
+		 * primitive values, in the background, Java instanciates a String Object.
+		 * Objects can have messages (Methods) which make them very usable 
+		 * and useful. Here is an exmple of a String object message. String.length()
+		 * returns an integer representation of the number of characters in the string.
+		 */
+
+		int numCharacters = name.length(); // Create an int (Primitive) variable and assign it the value returned by String.length();
 		
+		/*
+		 * Output the number of characters to Standard.out
+		 */
+		System.out.println("name has " + numCharacters + " characters.");
+
+		/*
+		 * Create a string object using the String class.
+		 */
+		String surName = new String("Smith");
+		
+		/*
+		 * Concatenate name and surname into one String.
+		 */
+		 String personName = name + " " + surName;
+		 
 		Person person1 = new Person(name);
+
+		//System.out.println(person1.name);
+
+		String[] firstNames = {"Dan","Chris","Dave","Bob","Ben","Steve","Carl","John","Kevin","muhammad"};
+
+		String[] lastNames = new String[10];
+
+		lastNames[0] = "Perkins";
+		lastNames[1] = "Smith"; 
+		lastNames[2] = "Jones"; 
+		lastNames[3] = "Schmidt"; 
+		lastNames[4] = "Sing"; 
+		lastNames[5] = "Thorpe"; 
+		lastNames[6] = "Watts"; 
+		lastNames[7] = "Evans"; 
+		lastNames[8] = "Jenkins"; 
+		lastNames[9] = "Green";
 		
-		System.out.println(person1.name);
-		
+		System.out.println(lastNames[5]);
+
 		int i;
-		
+
+		/*
+		 * Set upper and Lower limits for
+		 * Random number Generation.
+		 */
+		int low = 0;
+		int high = 9;
+
 		Person[] people = new Person[10];
-		
-		for(i = 0; i < 10; i++) {
-			
-			int fNameRef = 9 - i;
+
+		for (i = 0; i < 10; i++)
+		{
+
+			// Generate Random number
+			Random r = new Random();
+			int ranNum = r.nextInt(high - low) + low;
+
+			int fNameRef = ranNum;
 			String firstName = firstNames[fNameRef];
-			
+
 			int lNameRef = 9 - firstName.length();
-			
+
 			String lastName = lastNames[lNameRef];
-			
-			String fullName = firstName + lastName;
-			
+
+			String fullName = firstName + " " + lastName;
+
 			people[i] = new Person(fullName);
 		}
-		
-		for(Person person : people) {
-			
-			System.out.println(person.toString());
-		}
 
+		int count = 1;
+		for (Person person : people)
+		{
+
+			System.out.println("Name " + count + " : " + person.toString());
+			count ++;
+		}
 	}
 
 }
--- src/Person/Person.java
+++ src/Person/Person.java
@@ -7,5 +7,23 @@ public class Person {
 	public Person(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+
+		StringBuilder out = new StringBuilder();
+
+		/*out.append("Class: ");
+
+		out.append(this.getClass());
+
+		out.append("Person: ");
+
+		out.append(this.getName());*/
+
+		out.append(this.name);
+		
+		return out.toString();
+	}
 
 }
