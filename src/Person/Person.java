69e4a52c51814080dc31eff439111b3afc662ca8
danKrisdan <dan.krisdan@gmail.com>
2015-07-10 14:08:57 +0100

    created getter and setter for name.

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -5,11 +5,23 @@ public class Person {
 	private String name;
 
 	public Person(String name) {
+		this.setName(name);
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	private void setName(String name) {
 		this.name = name;
 	}
 	
-	public String getName(){
-		return this.name;
-	}
+
 
 }
